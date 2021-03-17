9d5b5bcd58a6c12b50bec097c3172f2084d732ba
cfff226 <cazziednb@outlook.com>
2021-03-17 12:35:16 +0000

    Add event listener to start function when DOM content has been loaded
    - Add function to create the game board

--- assets/spec/main.js
+++ assets/spec/main.js
@@ -1,6 +1,6 @@
 console.log("javascript's working");
 
-// variables for game cards // 
+// variables for the game cards // 
 
 var cardsChosen = [];
 var cardsChosenId = [];
@@ -10,3 +10,22 @@ var movesCount;
 var gameCard;
 var gameCardId;
 var displayButtons;
+
+// variables for the game information //
+
+var result = document.querySelector('result');
+
+// variables for each level //
+
+var restartButton = document.getElementById("restartButton");
+var easyButton = document.getElementById("easyButton");
+var normalButton = document.getElementById("normalButton");
+var hardButton = document.getElementById("hardButton");
+
+
+// array and function to create the game board //
+
+document.addEventListener('DOMContentLoaded', createBoard());
+
+function createBoard() {
+}
