9d7c8d10a60e18bc92ffaac8213997b633601d5f
cfff226 <cazziednb@outlook.com>
2021-03-17 12:25:03 +0000

    Add all variables to be used in the project

---
+++ spec/main.js
@@ -0,0 +1,12 @@
+console.log("javascript's working");
+
+// variables for game cards // 
+
+var cardsChosen = [];
+var cardsChosenId = [];
+var cardsWon = [];
+var numberOfCards;
+var movesCount;
+var gameCard;
+var gameCardId;
+var displayButtons;
